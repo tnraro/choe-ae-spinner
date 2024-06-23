3cc9bbe0deb9fa6f1b9fb4849b68a5a1a26af570
tnraro <deokji53@gmail.com>
2024-06-23 18:24:42 +0900

    add choe-ae picker

--- app/gallery.tsx
+++ app/gallery.tsx
@@ -1,9 +1,28 @@
-import { View, Text } from "react-native";
+import { selectedChoeAeAtom } from "@/constants/atoms/selected-choe-ae";
+import { launchImageLibraryAsync } from "expo-image-picker";
+import { useSetAtom } from "jotai";
+import { Button, Text, View } from "react-native";
 
 export default function ChoeAeGallery() {
+  const setSelectedChoeAe = useSetAtom(selectedChoeAeAtom);
+  const pickImage = async () => {
+    const result = await launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedChoeAe(result.assets[0].uri);
+    } else {
+      alert("Canceled");
+    }
+  }
   return (
     <View>
       <Text>Hi!</Text>
+      <Button
+        title="Add new choe-ae"
+        onPress={pickImage} />
     </View>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,17 @@
 import { Spinner } from "@/components/spinner";
+import { selectedChoeAeAtom } from "@/constants/atoms/selected-choe-ae";
 import { Link } from "expo-router";
+import { useAtomValue } from "jotai";
 import { StyleSheet, View } from "react-native";
 
 const PlaceholderImage = require("../assets/images/react-logo.png");
 
 export default function Index() {
+  const selectedChoeAe = useAtomValue(selectedChoeAeAtom);
+  const image = selectedChoeAe ? { uri: selectedChoeAe } : PlaceholderImage;
   return (
     <View style={styles.container}>
-      <Spinner image={PlaceholderImage} />
+      <Spinner image={image} />
       <Link href="/gallery">Gallery</Link>
     </View>
   );
